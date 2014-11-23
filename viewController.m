3a58a975015a4cb45792618730342d513551d745
CrazyCradleCats <jaesonvalles@gmail.com>
2014-11-23 18:38:51 -0500

    Update viewController.m

--- viewController.m
+++ viewController.m
@@ -16,54 +16,54 @@
     randomizer = 0;
     papaya.x = 160.0;
     papaya.y = 240.0;
+    papaya.center = CGPointMake(160, 240);
     step = [NSTimer scheduledTimerWithTimeInterval:(0.03) target:self selector:(onTimer) userInfo: nil repeats:YES];
 }
 
 - (void) onTimer{
     [self checkDist]
-    if(papaya.x < 0)
+    if(papaya.center.x < 0)
         pos.x = 5;
-    else if(papaya.x > 320)
+    else if(papaya.center.x > 320)
         pos.x = -5;
     else
         pos.x = xSpeed;
-        
-    if(papaya.y < 0)
+
+    if(papaya.center.y < 0)
         pos.y = 5;
-    else if(papaya.y > 320)
+    else if(papaya.center.y > 320)
         pos.y = -5;
     else
         pos.y = ySpeed;
-        
-    papaya.x = papaya.x + pos.x;
-    papaya.y = papaya.y + pos.y;
+
+    papaya.center = CGPointMake(papaya.center.x + pos.x, papaya.center.y + pos.y);
 }
 - (void) checkSpeed{
-    if(papaya.x - player.x > 0){
+    if(papaya.center.x - player.center.x > 0){
         xSpeed = 4;
-        if(papaya.x - player.x >= minDistX)
+        if(papaya.center.x - player.center.x >= minDistX)
             xSpeed = 0;
-    }else if(papaya.x - player.x < 0){
+    }else if(papaya.center.x - player.center.x < 0){
         xSpeed = -4;
-        if(papaya.x-player.x <= -1 * minDistX)
+        if(papaya.center.x-player.center.x <= -1 * minDistX)
             xSpeed = 0;
     }
     
-    if(papaya.y - player.y > 0){
+    if(papaya.center.y - player.center.y > 0){
         ySpeed = 4;
-        if(papaya.y - player.y >= minDistY)
+        if(papaya.center.y - player.center.y >= minDistY)
             ySpeed = 0;
-    }else if(papaya.y - player.y < 0){
+    }else if(papaya.center.y - player.center.y < 0){
         ySpeed = -4;
-        if(papaya.y - player.y <= -1 * minDistY)
+        if(papaya.center.y - player.center.y <= -1 * minDistY)
             ySpeed = 0;
     }
 }
         
 - (void)viewDidLoad {
     [super viewDidLoad];
-    step = [NSTimer scheduledTimerWithTimeInterval:(0.03) target:self selector:(onTimer) userInfo: nil repeats:YES];
-    pos = CGPointMake(0.0, 0.0);//Higher # = Faster x = horz & y = vert
+    step = [NSTimer scheduledTimerWithTimeInterval:(0.03) target:self selector:@selector(onTimer) userInfo: nil repeats:YES];
+    pos = CGPointMake(0.0, 0.0);
     xSpeed = 0;
     ySpeed = 0;
     minDistX = 64;
